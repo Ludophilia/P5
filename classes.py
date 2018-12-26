eb5e6e380eb3cb28162a5d6df37cafa284a7264b
Ludophilia <c1q4n93@outlook.com>
2018-12-26 12:33:33 +0100

    Apporte des améliorations au code de main.py pour qu'il fasse la même chose tout en prenant moins de place. Ajoute argparser pour notamment pouvoir activer ou desactiver la (re)creation db et le (re)remplissage des tables à chaque lancement de script

--- classes.py
+++ classes.py
@@ -3,28 +3,38 @@ import requests
 
 class Database : 
     
-    def __init__(self, user, password, host, use_unicode) : 
+    def __init__(self, user, password, host, use_unicode, verbosity) : 
         
         self.connection = mysql.connector.connect(
             user = user,
             password = password,
             host = host, 
-            use_unicode = use_unicode)
+            use_unicode = use_unicode) #Toujours comme un gros noob
           
         self.cursor = self.connection.cursor()
+        self.verbose = verbosity
 
     def create_database(self, sql_file, sep) : 
         with open(sql_file) as f : 
             for statement in f.read().split(sep) : 
                 self.cursor.execute(statement) 
+    
     def add_to_table(self, table_name, values) : 
+        
         extra_statement = {
         "5db.Categorie" : " (nom) VALUES (%s)", 
         "5db.Nutriscore" : " (grade) VALUES (%s)",
         "5db.Produit" : " (nom, description, quantite, off_url, category_name, retailer_name, nutriscore) VALUES (%(product_name)s, %(ingredients_text)s, %(quantity)s, %(url)s, %(categories)s, %(stores)s, %(nutrition_grade_fr)s)"} 
+        
         addtotable_statement = ("INSERT INTO " + table_name + extra_statement[table_name]) 
 
-        self.cursor.execute(addtotable_statement, values)
+        try : 
+            self.cursor.execute(addtotable_statement, values)
+            self.connection.commit()
+
+        except mysql.connector.errors.IntegrityError :
+            if self.verbose == True :
+                print("Item alrealdy inserted in {}, moving on".format(table_name))
 
     def close_cursor(self) : 
         return self.cursor.close()
@@ -49,10 +59,11 @@ class Structural_data :
 
 class Product_data :
     
-    def __init__ (self, search_terms, page_size, page_number) : 
+    def __init__ (self, search_terms, page_size, page_number, verbosity) : 
         self.search_pr = {"action" : "process", "search_terms" : search_terms, "sort_by" : "unique_scans_n", "page_size" : page_size, "page" : page_number, "json": "true"}
         self.data = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params = self.search_pr)
         self.keys_to_check = ["product_name", "ingredients_text", "quantity", "url", "categories", "stores", "nutrition_grade_fr"]
+        self.verbose = verbosity
 
     @property    
     def listversion (self) :
@@ -74,16 +85,20 @@ class Product_data :
             try :
                 if len(product[value_tested]) == 0 :
                     keys_missing[value_tested] = "this key returns no value for product " + product["product_name"]
-                    print(keys_missing) 
+                    
+                    if self.verbose == True :
+                        print(keys_missing) 
 
             except KeyError : 
                 try :
                     key_problem = True
                     keys_missing[value_tested] = "WARNING : this key does not exist in product " + product["product_name"] 
-                    print(keys_missing)
+                    if self.verbose == True :
+                        print(keys_missing) 
                 except KeyError : 
                     key_problem = True
                     keys_missing[value_tested] = "WARNING : this key does not exist in this product. Unable to give product's name because its key does not exist."
-                    print(keys_missing)
+                    if self.verbose == True :
+                        print(keys_missing) 
 
         return True if "nutrition_grade_fr" in keys_missing or "product_name" in keys_missing or key_problem == True else False
