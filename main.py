ce701c17b29db892b1f7a78752884a87dc6e8d4d
Ludophilia <c1q4n93@outlook.com>
2018-12-28 14:56:08 +0100

    Commence le test de l'interface utilisateur

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 from classes import *
 
 parser = argparse.ArgumentParser(description="Main script for Projet P5. Help you to improve your diet.")
@@ -7,15 +8,16 @@ parser.add_argument('-b', '--build_db', help = "Enable or disable database creat
 parser.add_argument('-v', '--verbose', help = "Make table filling and data testing talkative", action = "store_true")
 
 args = parser.parse_args()
-# print(args)
+print(args)
 
 def main() : 
     database = Database("p5", "12345", "127.0.0.1", True, args.verbose)
-    
-    nutriscores = Structural_data("nutriscores.txt")
-    categories = Structural_data("categories-short.txt")
 
-    if args.build_db == True : #il faut peut-être utiliser argparser pour activer et desactiver la création et le remplissage de la db sur commande 
+    if args.build_db == True :  
+        
+        nutriscores = Structural_data("nutriscores.txt") 
+        categories = Structural_data("categories-short.txt") #On en aura peut être besoin de nouveau, pour le moment, ça reste ici...
+
         database.create_database("database.sql", ";")
     
         for nutriscore in nutriscores.listversion :
@@ -30,9 +32,40 @@ def main() :
                 if products.key_tester(product) == False : 
                     database.add_to_table("5db.Produit", products.prepared_for_insertion(category, product))
                     
-    """ Test de l'interface utilisateur """ #A poor use of docstrings, isn't it?
+    ############################ Test de l'interface utilisateur ##############################
+    
+    def test_input(user_input, prompt) : #Separer le test et la valeur renvoyée
 
-    print('') 
+        while type(user_input) != int : #Prochaine étape, faire en sorte que la fonction
+            try :
+                user_input = int(user_input)
+            
+            except ValueError : 
+                print(("Le caractère que vous avez entré n'est pas un chiffre ou un nombre.\n"
+                "Veuillez entrer un nombre s'il vous plait"))
+                time.sleep(2)
+                user_input = input(prompt) 
+
+   # Obj : Coder l'interface utilisateur ! On l'a designé dans un document à part, il faut juste réaliser ce qu'on a pensé...
+    
+    menu_prompt = ("\nBienvenue sur P5!\n"
+        "Menu principal :\n"
+        "1. Remplacer un aliment\n"
+        "2. Retrouver mes aliments remplacés\n\n")
+
+    error_prompt = ("Le caractère que vous avez entré n'est pas un chiffre ou un nombre.\n"
+    "Veuillez entrer un nombre s'il vous plait")
+
+    user_input = input(menu_prompt)
+
+    if test_input(user_input, menu_prompt) == 1 :  
+        print("OK")
+    
+    elif test_input(user_input, menu_prompt) == 2 :
+        print("OK")
+    
+    else :
+        print("PAS OK")
 
     database.close_cursor()
 
